b2d88a9f5124d26a1ae3f4a9abba54dd99245522
Sarah Eiting <sarah.eiting@gmail.com>
2014-11-10 10:45:15 -0800

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,8 +3,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 import sys, os, time
-from .server_tools import reset_database
 from datetime import datetime
+from django.conf import settings
+from .server_tools import reset_database, create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
+
 
 SCREEN_DUMP_LOCATION = os.path.abspath(
 	os.path.join(os.path.dirname(__file__), 'screendumps')
@@ -111,3 +114,17 @@ class FunctionalTest(StaticLiveServerTestCase):
 	    self.wait_for_element_with_id('id_login')
 	    navbar = self.browser.find_element_by_css_selector('.navbar')
 	    self.assertNotIn(email, navbar.text)
+
+	def create_pre_authenticated_session(self, email):
+		if self.against_staging:
+			session_key = create_session_on_server(self.server_host, email)
+		else:
+			session_key = create_pre_authenticated_session(email)
+		## to set a cookie we need to first visit the domain.
+		## 404 pages load the quickest
+		self.browser.get(self.server_url + "/404_no_such_url/")
+		self.browser.add_cookie(dict(
+			name=settings.SESSION_COOKIE_NAME,
+			value=session_key,
+			path='/',
+		))
